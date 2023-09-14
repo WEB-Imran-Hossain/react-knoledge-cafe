d5c60eb20bbbbb9299484b90ff5d279ca682c0e9
Imran <webimran2021@gmail.com>
2023-09-14 21:51:46 +0600

    adding blog data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Blogs from './components/Blogs/blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-6xl '>Knowledge Cafe</h1>
+      <Header></Header>
+      <div className='md:flex'>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
       
       
     </>
